4469d8da938500c809ca24f647d5af0cca9f7609
Nicholas Samson <slicknd07@gmail.com>
2024-09-16 19:48:35 -0600

    bishopMovesCalculator completion and some of queenMovesCalculator

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -41,10 +41,7 @@ public class PieceMovesCalculator {
 
     private Collection<? extends ChessMove> KingMovesCalculator() {
         Collection<ChessMove> moves = new HashSet<>();
-        int[][] kingDirections = {
-                {-1,-1}, {-1,0},{-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}
-        };
-
+        int[][] kingDirections = { {-1,-1}, {-1,0},{-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}};
         for(int[] dir : kingDirections) {
             int nextRow = position.getRow() + dir[0];
             int nextCol = position.getColumn() + dir[1];
@@ -58,22 +55,58 @@ public class PieceMovesCalculator {
                 }
             }
         }
-
-
-
         return moves;
     }
 
     private Collection<? extends ChessMove> QueenMovesCalculator() {
-        return new ArrayList<>();
+        Collection<ChessMove> moves = new HashSet<>();
+        int[][] queenDirections = { {-1,-1}, {-1,0},{-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}};
+        for(int[] dir : queenDirections) {
+            int nextRow = position.getRow() + dir[0];
+            int nextCol = position.getColumn() + dir[1];
+
+            if(nextRow >= 1 && nextRow <= 8 && nextCol >= 1 && nextCol <= 8) {
+                ChessPosition newPosition = new ChessPosition(nextRow, nextCol);
+                ChessPiece newPiece = board.getPiece(newPosition);
+
+                if(newPiece == null || !newPiece.getTeamColor().equals(this.board.getPiece(position).getTeamColor())) {
+                    moves.add(new ChessMove(this.position, newPosition, null));
+                }
+            }
+        }
+        return moves;
     }
 
     private Collection<? extends ChessMove> BishopMovesCalculator() {
-        for(int i = position.getRow(); i <= 8; i++) {
-            for(int j = position.getColumn(); j <= 8; j++) {
+        Collection<ChessMove> moves = new HashSet<>();
+        int[][] bishopDirections = { {1,1}, {1,-1}, {-1,1}, {-1,-1}};
+
+        for(int[] dir : bishopDirections) {
+            int row = position.getRow() - 1;
+            int col = position.getColumn() - 1;
+
+            while (true) {
+                row += dir[0];
+                col += dir[1];
+
+                if(row < 0 || row >= 8 || col < 0 || col >= 8) {
+                    break;
+                }
+
+                ChessPosition newPosition = new ChessPosition(row + 1, col + 1);
+                ChessPiece piece = board.getPiece(newPosition);
+
+                if (piece == null) {
+                    moves.add(new ChessMove(position, newPosition,null)); // Empty square
+                } else if (!piece.getTeamColor().equals(this.board.getPiece(position).getTeamColor())) {
+                    moves.add(new ChessMove(position, newPosition,null)); // Capture opponent's piece
+                    break; // Cannot move past the captured piece
+                } else {
+                    break; // Blocked by own piece
+                }
             }
         }
-        return new ArrayList<>();
+        return moves;
     }
 
     private Collection<? extends ChessMove> KnightMovesCalculator() {
