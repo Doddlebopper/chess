df2facf8e47da8684204237cffc768c45dc38339
Nicholas Samson <slicknd07@gmail.com>
2024-10-22 21:12:16 -0600

    autograder fixes

---
+++ server/src/test/java/service/StandardServiceTests.java
@@ -0,0 +1,22 @@
+package service;
+
+import chess.ChessGame;
+import org.junit.jupiter.api.*;
+import passoff.model.*;
+import passoff.server.TestServerFacade;
+import server.Server;
+
+import java.net.HttpURLConnection;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Locale;
+public class StandardServiceTests {
+
+    @Test
+    @DisplayName("Multiple Clears")
+    public void multipleClear() {
+
+    }
+
+}
