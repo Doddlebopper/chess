5f9604a1419ceb3b95cf9822d8bc638469383d71
Nicholas Samson <slicknd07@gmail.com>
2024-11-26 10:43:12 -0700

    MakeMove UserGameCommand

---
+++ shared/src/main/java/websocket/commands/MakeMove.java
@@ -0,0 +1,21 @@
+package websocket.commands;
+
+import chess.ChessMove;
+
+public class MakeMove extends UserGameCommand {
+
+    ChessMove move;
+
+    public MakeMove(String authToken, int gameID, ChessMove move) throws IllegalAccessException {
+        super(CommandType.MAKE_MOVE, authToken, gameID);
+        if(move == null) {
+            throw new IllegalAccessException("Move cannot be null");
+        }
+        this.move = move;
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
+
+}
